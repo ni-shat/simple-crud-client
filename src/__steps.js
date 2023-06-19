0e97c69ee4544da1bf5894c306d65cc16922f5d0
ni-shat <nishat.tabassum301@gmail.com>
2023-06-19 12:40:59 +0600

    simple crud client

---
+++ src/__steps.js
@@ -0,0 +1,5 @@
+/***
+ * first kaj backend e data pathanur jonno, back end e ekta api create korte hobe. 
+ * 
+ * So post diye eita korbo. So korar por back end e post er jonno ekta route set kore felsi
+ */
